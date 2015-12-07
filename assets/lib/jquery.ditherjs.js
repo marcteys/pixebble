73aa43bca5a1c334daa426d6bd6c0dd4bb63f116
Marc Teyssier <marc.teys@gmail.com>
2015-12-07 12:17:59 +0100

    feat(Design): Adding design elements

--- assets/lib/jquery.ditherjs.js
+++
@@ -1,9 +0,0 @@
-/**
-* jQuery plugin definition
-* */
-;(function( $ ) {
-    $.fn.ditherJS = function(opt, callback) {
-            new DitherJS(this.selector,opt, callback);            
-        return this;
-    };
-}( jQuery ));
