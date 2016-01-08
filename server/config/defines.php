e9b7733da9619b9d289b1e66146bea30f1382840
Marc Teyssier <marc.teys@gmail.com>
2016-01-08 15:35:10 +0100

    clean(Watchapp): Clean watchapp branch

--- server/config/defines.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
-ini_set('error_reporting', E_ALL);
-define('UPLOAD_DIR', './../data/uploads/');
-define('UPLOAD_DIR_ABS', 'data/uploads/');
--- server/config/startup.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-include("defines.php");
-include('./controllers/functions.php');
-include('./models/Images.php');
-
-$_POST   = array_map("filterData", $_POST);
-$_GET    = array_map("filterData", $_GET);
-$_COOKIE = array_map("filterData", $_COOKIE);
